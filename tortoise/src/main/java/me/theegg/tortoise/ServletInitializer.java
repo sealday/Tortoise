7b01a5c10b3f6a71e981690dc9a1474d881e35af
sealday <sealday@gmail.com>
2015-06-16 22:17:29 +0800

    init commit

---
+++ tortoise/src/main/java/me/theegg/tortoise/ServletInitializer.java
@@ -0,0 +1,13 @@
+package me.theegg.tortoise;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TortoiseApplication.class);
+	}
+
+}
---
+++ tortoise/src/main/java/me/theegg/tortoise/TortoiseApplication.java
@@ -0,0 +1,12 @@
+package me.theegg.tortoise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TortoiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TortoiseApplication.class, args);
+    }
+}
